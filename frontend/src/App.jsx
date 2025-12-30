b269112da4a0195baaa6c2b6b82cd1326477e6a6
Ahmadmuneer Abdulqudus <itopa1199@gmail.com>
2025-12-30 19:56:18 +0100

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,25 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import Register from './pages/Register.jsx' 
-import Login from './pages/Login.jsx'
-import { set } from 'react-hook-form'
-import axios from 'axios'
-import { useState } from 'react'
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+import { set } from "react-hook-form";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  
   return (
     <>
-     <Router>
-      <div>
-        <Routes>
-        <Route path="/" element={<Home  />} />
-        <Route path="/register" element={<Register />} />
-        <Route path='/login' element= {<Login />}/>
-      </Routes>
-      </div>
-     </Router>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
